c050cff0705c4080181df087c5d939422051583a
Luis Angel <luis9925.la@gmail.com>
2023-04-07 18:50:46 -0600

    modifica diseño de boton

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
+import 'dart:math';
 
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   late GoogleMapController mapController;
   late LatLng _initialLocation = const LatLng(0.0, 0.0);
   String message = '';
@@ -46,9 +46,9 @@ class _HomeState extends State<Home> {
 
     if (!serviceEnabled) {
       if (context.mounted) {
-         ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: 
-            Text('Los servicios de ubicacion estan desactivados. Por favor activarlos')));
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text(
+                'Los servicios de ubicacion estan desactivados. Por favor activarlos')));
       }
       return false;
     }
@@ -59,9 +59,8 @@ class _HomeState extends State<Home> {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
         if (context.mounted) {
-          ScaffoldMessenger.of(context).showSnackBar(
-            const SnackBar(content: 
-              Text('Los servicios de ubicacion estan negados')));
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+              content: Text('Los servicios de ubicacion estan negados')));
         }
         return false;
       }
@@ -69,8 +68,9 @@ class _HomeState extends State<Home> {
 
     if (permission == LocationPermission.deniedForever) {
       if (context.mounted) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          const SnackBar(content: Text('Los servicios de ubicacion estan desactivados permanenntemente.')));
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text(
+                'Los servicios de ubicacion estan desactivados permanenntemente.')));
       }
       return false;
     }
@@ -82,12 +82,22 @@ class _HomeState extends State<Home> {
     final hasPermission = await _handleLocationPermission();
 
     if (hasPermission) {
-      await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high,)
-        .then((Position position) => {
+      await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      )
+          .then((Position position) => {
                 setState(() => {
-                      user = UserData(idDevice: idDevice, lat: position.latitude, lon: position.longitude, tsGps: position.timestamp!.microsecondsSinceEpoch),
-                      _initialLocation = LatLng(position.latitude, position.longitude),
-                      mapController.animateCamera(CameraUpdate.newLatLngZoom(LatLng(_initialLocation.latitude, _initialLocation.longitude), 14))
+                      user = UserData(
+                          idDevice: idDevice,
+                          lat: position.latitude,
+                          lon: position.longitude,
+                          tsGps: position.timestamp!.microsecondsSinceEpoch),
+                      _initialLocation =
+                          LatLng(position.latitude, position.longitude),
+                      mapController.animateCamera(CameraUpdate.newLatLngZoom(
+                          LatLng(_initialLocation.latitude,
+                              _initialLocation.longitude),
+                          14))
                     }),
               })
           .catchError((e) {
@@ -97,32 +107,36 @@ class _HomeState extends State<Home> {
   }
 
   void _onLocationChange() async {
-
     const LocationSettings locationSettings = LocationSettings(
       accuracy: LocationAccuracy.high,
       distanceFilter: 10,
     );
 
     if (_positionStreamSubscription == null) {
-      _positionStreamSubscription = Geolocator.getPositionStream(locationSettings: locationSettings)
-        .listen((Position position) {
-            print('Location Change Active');
-            user = UserData(idDevice: idDevice, lat: position.latitude, lon: position.longitude, tsGps: position.timestamp!.microsecondsSinceEpoch);
-            message = 'ID: ${user.idDevice} Latitud: ${user.lat}  Longitud: ${user.lon} GPSTime: ${user.tsGps}';
-            print(message);
-            _sendDataToVidente(user);
-            mapController.animateCamera(CameraUpdate.newLatLngZoom(LatLng(position.latitude, position.longitude), 14));
-            setState(() => {
-              locationStatus = true
-          });
-        }); 
+      _positionStreamSubscription =
+          Geolocator.getPositionStream(locationSettings: locationSettings)
+              .listen((Position position) {
+        print('Location Change Active');
+        user = UserData(
+            idDevice: idDevice,
+            lat: position.latitude,
+            lon: position.longitude,
+            tsGps: position.timestamp!.microsecondsSinceEpoch);
+        message =
+            'ID: ${user.idDevice} Latitud: ${user.lat}  Longitud: ${user.lon} GPSTime: ${user.tsGps}';
+        print(message);
+        _sendDataToVidente(user);
+        mapController.animateCamera(CameraUpdate.newLatLngZoom(
+            LatLng(position.latitude, position.longitude), 14));
+        setState(() => {locationStatus = true});
+      });
     } else {
-        print('Location Change Desactive');
-        _positionStreamSubscription!.cancel();
-        _positionStreamSubscription = null;
-        setState(() {
-          locationStatus = false;
-        });
+      print('Location Change Desactive');
+      _positionStreamSubscription!.cancel();
+      _positionStreamSubscription = null;
+      setState(() {
+        locationStatus = false;
+      });
     }
   }
 
@@ -139,7 +153,8 @@ class _HomeState extends State<Home> {
 
   Future<void> _sendDataToVidente(UserData userData) async {
     String body = jsonEncode(userData.toJson());
-    var url = Uri.parse('');
+    var url = Uri.parse(
+        '');
     var response = await http.post(url, body: body);
 
     if (response.statusCode == 200) {
@@ -153,56 +168,91 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: TextButton(
-          onPressed: _onLocationChange,
-          style: ButtonStyle(
-            padding: MaterialStateProperty.all(const EdgeInsets.all(12)),
-            foregroundColor: MaterialStateProperty.all(Colors.indigo),
-            backgroundColor: MaterialStateProperty.all(Colors.indigo),
-            shape: MaterialStateProperty.all(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(22.0),
-                side: const BorderSide(color: Colors.indigo)
-              )
-            )
-          ),
-          child: 
-            locationStatus ?  
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Icon(
-                    Icons.stop,
-                    color: Colors.white,
-                    ),
-                  Text(
-                    style: TextStyle(color: Colors.white),
-                    'Detener'
-                  )
-                ],
-              ): 
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Icon(Icons.play_arrow, color: Colors.white,),
-                  Text(
-                    style: TextStyle(color: Colors.white),
-                    '  Comenzar')
-                ],
-              ),
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       body: Stack(
         children: [
-          Positioned(child: GoogleMap(
+          Positioned.fill(
+              child: GoogleMap(
             onMapCreated: _onMapCreated,
             initialCameraPosition: CameraPosition(target: _initialLocation),
             myLocationEnabled: true,
-            myLocationButtonEnabled: true,
-          ))
+            zoomControlsEnabled: false,
+          )),
+          Positioned(
+              bottom: 30,
+              left: 0,
+              right: 0,
+              child: Container(
+                padding: const EdgeInsets.all(12),
+                margin: const EdgeInsets.only(
+                    top: 10, bottom: 10, left: 20, right: 20),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(100),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black.withOpacity(0.2),
+                          blurRadius: 10,
+                          offset: Offset.zero)
+                    ]),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          print('Presionando boton');
+                        },
+                        icon: const Icon(Icons.person)),
+                    Text(
+                      'user${Random().nextInt(1000)}',
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.indigo,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: _onLocationChange,
+                      style: ButtonStyle(
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.all(12)),
+                          foregroundColor:
+                              MaterialStateProperty.all(Colors.indigo),
+                          backgroundColor:
+                              MaterialStateProperty.all(Colors.indigo),
+                          shape: MaterialStateProperty.all(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(22.0),
+                                  side:
+                                      const BorderSide(color: Colors.indigo)))),
+                      child: locationStatus
+                          ? Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: const [
+                                Icon(
+                                  Icons.stop,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                    style: TextStyle(color: Colors.white),
+                                    'Detener')
+                              ],
+                            )
+                          : Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: const [
+                                Icon(
+                                  Icons.play_arrow,
+                                  color: Colors.white,
+                                ),
+                                Text(
+                                    style: TextStyle(color: Colors.white),
+                                    '  Comenzar')
+                              ],
+                            ),
+                    ),
+                  ],
+                ),
+              )),
         ],
       ),
     );
